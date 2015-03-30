ef48f9485ff26477ff79520c0c176328c18b008b
Bill Kilmer <wdk122@gmail.com>
2015-03-30 16:03:11 -0700

    Makes HR logo move in random directions.

--- src/hRDancer.js
+++ src/hRDancer.js
@@ -7,5 +7,32 @@ HRDancer.prototype = Object.create(Dancer.prototype);
 HRDancer.prototype.constructor = HRDancer;
 HRDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
-  this.$node.fadeToggle();
+
+  var randY = $("body").height() / 10 * (2 * Math.random() - 1);
+  var randX = $("body").width() / 10 * (2 * Math.random() - 1);
+
+
+  setTimeout(function(){
+    Dancer.prototype.setPosition.call(this, this.$node.position().top + randY,
+    this.$node.position().left +  randX);
+  }.bind(this), 50);
 };
+
+// bill
+
+  // var randY = $("body").height() / 10 * Math.random();
+  // var randX = $("body").width() / 10 * Math.random(); 
+
+
+  // setTimeout(function(){
+  //   Dancer.prototype.setPosition.call(this, this.$node.position().top + randY,
+  //   this.$node.position().left +  randX);
+  // }.bind(this), 1000);
+
+  // michael
+
+  // setTimeout(function(){
+  //   console.log(this.$node.position().top);
+  //   debugger;
+  //   Dancer.prototype.setPosition.call(this, randY, randX);
+  // }.bind(this), 1000);
